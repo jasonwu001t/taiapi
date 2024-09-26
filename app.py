75687423a71f35b484a369ba3c2f8e37b56a1a70
jasonwu001t <jasonwu001t@gmail.com>
2024-09-25 21:13:14 -0700

    j

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Chalice(app_name='taiapi')
 # Initialize boto3 client for S3
 s3_client = boto3.client('s3')
 
-BUCKET_NAME = 'jtrade1-dir'  # S3 bucket name
+BUCKET_NAME = 'jtrade1-dir'  # S3 bucket name for the app
+SUBSCRIPTION_KEY = 'subscriptions.json'  # Key for storing subscription data
 
 # Define FRED and BLS S3 keys outside functions to avoid redundancy
 FRED_S3_KEYS = {
@@ -61,6 +62,13 @@ def create_json_response(body, status_code=200):
         status_code=status_code,
         headers={'Content-Type': 'application/json'}
     )
+
+# Helper function to upload JSON data to S3
+def upload_json_to_s3(key: str, data: dict):
+    try:
+        s3_client.put_object(Bucket=BUCKET_NAME, Key=key, Body=json.dumps(data))
+    except ClientError as e:
+        return {"error": f"Unable to upload {key} to S3: {str(e)}"}
 
 # Date filtering function for FRED and BLS data
 def filter_last_10_years(data, date_field="date"):
@@ -224,283 +232,43 @@ def get_daily_ohlc(symbol):
     return create_json_response(data)
 
 
-############################################################################################################
-### BELOW IS HOW THIS API WAS HANDLED BEFORE, MOVED TO ABOVE METHOD
-############################################################################################################
+#########################
+# POST subscription endpoint
+#########################
 
-
-# from chalice import Chalice, Response
-# import boto3
-# import json
-# from botocore.exceptions import ClientError
-# from datetime import datetime
-
-# app = Chalice(app_name='taiapi')
-
-# # Initialize boto3 client for S3
-# s3_client = boto3.client('s3')
-
-# BUCKET_NAME = 'jtrade1-dir'  # S3 bucket name
-
-# # Helper function to fetch JSON data from S3
-# def fetch_json_from_s3(key: str):
-#     """Fetch JSON data from S3 bucket."""
-#     try:
-#         response = s3_client.get_object(Bucket=BUCKET_NAME, Key=key)
-#         content = response['Body'].read().decode('utf-8')
-#         return json.loads(content)
-#     except ClientError as e:
-#         return {"error": f"Unable to fetch {key} from S3: {str(e)}"}
-#     except json.JSONDecodeError:
-#         return {"error": f"Error decoding JSON in {key}"}
-
-# # Helper function to create JSON responses
-# def create_json_response(body, status_code=200):
-#     return Response(
-#         body=body,
-#         status_code=status_code,
-#         headers={'Content-Type': 'application/json'}
-#     )
-
-# @app.route('/us_treasury_yield', methods=['GET'], cors=True)
-# def get_categories():
-#     categories = fetch_json_from_s3('api/treasury_yield_all.json')
-#     if "error" in categories:
-#         return create_json_response({'message': 'Internal server error', 'details': categories["error"]}, status_code=500)
-#     return create_json_response(categories)
-
-# # Endpoint to get all articles or filter by id
-# @app.route('/articles', methods=['GET'], cors=True)
-# @app.route('/articles/{id}', methods=['GET'], cors=True)
-# def get_articles(id=None):
-#     articles = fetch_json_from_s3('articles.json')
-#     if "error" in articles:
-#         return create_json_response({'message': 'Internal server error', 'details': articles["error"]}, status_code=500)
-
-#     if id:
-#         # Filter articles by id
-#         article = next((a for a in articles if a['id'] == id), None)
-#         if article:
-#             return create_json_response(article)
-#         else:
-#             return create_json_response({'message': f'Article with id {id} not found'}, status_code=404)
-
-#     return create_json_response(articles)
-
-# # Endpoint to get all chart data or filter by id
-# @app.route('/chart', methods=['GET'], cors=True)
-# @app.route('/chart/{id}', methods=['GET'], cors=True)
-# def get_chart_data(id=None):
-#     chart_data = fetch_json_from_s3('chart_data.json')
-#     if "error" in chart_data:
-#         return create_json_response({'message': 'Internal server error', 'details': chart_data["error"]}, status_code=500)
-
-#     if id:
-#         # Filter chart data by id
-#         chart = next((c for c in chart_data if c['id'] == id), None)
-#         if chart:
-#             return create_json_response(chart)
-#         else:
-#             return create_json_response({'message': f'Chart with id {id} not found'}, status_code=404)
-
-#     return create_json_response(chart_data)
-
-# # Endpoint to get all categories from S3
-# @app.route('/categories', methods=['GET'], cors=True)
-# def get_categories():
-#     categories = fetch_json_from_s3('categories.json')
-#     if "error" in categories:
-#         return create_json_response({'message': 'Internal server error', 'details': categories["error"]}, status_code=500)
-#     return create_json_response(categories)
-
-# # Endpoint to get all indicators from S3
-# @app.route('/indicators', methods=['GET'], cors=True)
-# def get_indicators():
-#     indicators = fetch_json_from_s3('indicators.json')
-#     if "error" in indicators:
-#         return create_json_response({'message': 'Internal server error', 'details': indicators["error"]}, status_code=500)
-#     return create_json_response(indicators)
-
-# # Updated endpoint to get daily OHLC data with date filters
-# @app.route('/stocks/daily_ohlc/{symbol}', methods=['GET'], cors=True)
-# def get_daily_ohlc(symbol):
-#     if symbol:
-#         key = f'api/stock_daily_bar/{symbol.upper()}.json'
-#         data = fetch_json_from_s3(key)
-#         if "error" in data:
-#             return create_json_response(
-#                 {'message': f'Stock data for {symbol.upper()} not found', 'details': data["error"]},
-#                 status_code=404
-#             )
-#         else:
-#             # Get query parameters
-#             request = app.current_request
-#             query_params = request.query_params
-
-#             # Initialize date filters
-#             start_date = None
-#             end_date = None
-
-#             # Parse 'from' and 'to' query parameters
-#             if query_params:
-#                 from_str = query_params.get('from')
-#                 to_str = query_params.get('to')
-
-#                 date_format = "%Y-%m-%d"
-
-#                 if from_str:
-#                     try:
-#                         start_date = datetime.strptime(from_str, date_format).date()
-#                     except ValueError:
-#                         return create_json_response(
-#                             {'message': "Invalid 'from' date format. Expected YYYY-MM-DD."},
-#                             status_code=400
-#                         )
-
-#                 if to_str:
-#                     try:
-#                         end_date = datetime.strptime(to_str, date_format).date()
-#                     except ValueError:
-#                         return create_json_response(
-#                             {'message': "Invalid 'to' date format. Expected YYYY-MM-DD."},
-#                             status_code=400
-#                         )
-
-#             # Filter data by date if filters are provided
-#             if start_date or end_date:
-#                 filtered_data = []
-#                 for record in data:
-#                     # Parse the date in each record
-#                     try:
-#                         record_date = datetime.strptime(record['date'], "%Y-%m-%d").date()
-#                     except ValueError:
-#                         continue  # Skip records with invalid date format
-
-#                     # Apply the date filters
-#                     if start_date and record_date < start_date:
-#                         continue
-#                     if end_date and record_date > end_date:
-#                         continue
-#                     filtered_data.append(record)
-#                 data = filtered_data
-
-#             return create_json_response(data)
-#     else:
-#         return create_json_response({'message': 'Symbol is required'}, status_code=400)
+@app.route('/subscribe', methods=['POST'], cors=True)
+def subscribe_user():
+    request = app.current_request
+    body = request.json_body
     
-# #################  BLS ############################
-# @app.route('/unemployment_rate', methods=['GET'], cors=True)  #data from BLS
-# def get_unemployment_rate():
-#     data = fetch_json_from_s3('api/bls/unemployment_rate.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
+    # Validate the input data
+    name = body.get('name')
+    email = body.get('email')
+    subscribe_to_email_list = body.get('subscribe', False)  # Default to False if not provided
+    
+    if not name or not email:
+        return create_json_response({'message': 'Name and email are required.'}, status_code=400)
+    
+    # Fetch existing subscription data from S3
+    subscriptions = fetch_json_from_s3(SUBSCRIPTION_KEY)
+    if "error" in subscriptions:
+        return create_json_response({'message': 'Internal server error', 'details': subscriptions['error']}, status_code=500)
+    
+    # Add the new subscription
+    new_subscription = {
+        'name': name,
+        'email': email,
+        'subscribe': subscribe_to_email_list
+    }
+    
+    if isinstance(subscriptions, list):
+        subscriptions.append(new_subscription)
+    else:
+        subscriptions = [new_subscription]
 
-# @app.route('/nonfarm_payroll', methods=['GET'], cors=True)  #data from BLS
-# def get_nonfarm_payroll():
-#     data = fetch_json_from_s3('api/bls/nonfarm_payroll.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
+    # Upload updated subscription list to S3
+    upload_error = upload_json_to_s3(SUBSCRIPTION_KEY, subscriptions)
+    if upload_error:
+        return create_json_response({'message': 'Internal server error', 'details': upload_error['error']}, status_code=500)
 
-# @app.route('/us_avg_weekly_hours', methods=['GET'], cors=True)  #data from BLS
-# def get_us_avg_weekly_hours():
-#     data = fetch_json_from_s3('api/bls/us_avg_weekly_hours.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/us_job_opening', methods=['GET'], cors=True)  #data from BLS
-# def get_us_job_opening():
-#     data = fetch_json_from_s3('api/bls/us_job_opening.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-# ###################################################
-
-
-# #################  FRED ############################
-# @app.route('/us_30yr_fix_mortgage_rate', methods=['GET'], cors=True) 
-# def get_us_30yr_fix_mortgage_rate():
-#     data = fetch_json_from_s3('api/fred/us_30yr_fix_mortgage_rate.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/consumer_price_index', methods=['GET'], cors=True) 
-# def get_consumer_price_index():
-#     data = fetch_json_from_s3('api/fred/consumer_price_index.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/federal_funds_rate', methods=['GET'], cors=True) 
-# def get_federal_funds_rate():
-#     data = fetch_json_from_s3('api/fred/federal_funds_rate.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/gdp', methods=['GET'], cors=True)  
-# def get_gdp():
-#     data = fetch_json_from_s3('api/fred/gdp.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/core_cpi', methods=['GET'], cors=True) 
-# def get_core_cpi():
-#     data = fetch_json_from_s3('api/fred/core_cpi.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/fed_total_assets', methods=['GET'], cors=True) 
-# def get_fed_total_assets():
-#     data = fetch_json_from_s3('api/fred/fed_total_assets.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/m2', methods=['GET'], cors=True)
-# def get_m2():
-#     data = fetch_json_from_s3('api/fred/m2.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/unemployment_rate', methods=['GET'], cors=True)  #data from BLS
-# def get_unemployment_rate():
-#     data = fetch_json_from_s3('api/fred/unemployment_rate.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/sp500', methods=['GET'], cors=True) 
-# def get_sp500():
-#     data = fetch_json_from_s3('api/fred/sp500.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/commercial_banks_deposits', methods=['GET'], cors=True)
-# def get_commercial_banks_deposits():
-#     data = fetch_json_from_s3('api/fred/commercial_banks_deposits.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/total_money_market_fund', methods=['GET'], cors=True) 
-# def get_total_money_market_fund():
-#     data = fetch_json_from_s3('api/fred/total_money_market_fund.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-
-# @app.route('/us_producer_price_index', methods=['GET'], cors=True) 
-# def get_us_producer_price_index():
-#     data = fetch_json_from_s3('api/fred/us_producer_price_index.json')
-#     if "error" in data:
-#         return create_json_response({'message': 'Internal server error', 'details': data["error"]}, status_code=500)
-#     return create_json_response(data)
-# ###################################################
+    return create_json_response({'message': 'Subscription successful.'}, status_code=200)
--- test.py
+++ test.py
@@ -1,34 +1,20 @@
-from chalice import Chalice, Response
-import boto3
-import json
-from botocore.exceptions import ClientError
+import requests
 
-app = Chalice(app_name='taiapi')
+# Define the API URL (replace with your actual URL)
+api_url = 'https://bmohn85r9a.execute-api.us-west-2.amazonaws.com/api/subscribe'
 
-# Initialize boto3 client for S3
-s3_client = boto3.client('s3')
+# Define the data to be sent in the POST request
+data = {
+    'name': 'John Doe',
+    'email': 'john.doe@example.com',
+    'subscribe': True  # or False if the user does not want to subscribe to the email list
+}
 
-BUCKET_NAME = 'jtrade1-dir'  # S3 bucket name
+# Send the POST request
+response = requests.post(api_url, json=data)
 
-# Helper function to fetch JSON data from S3
-def fetch_json_from_s3(key: str):
-    """Fetch JSON data from S3 bucket."""
-    try:
-        response = s3_client.get_object(Bucket=BUCKET_NAME, Key=key)
-        content = response['Body'].read().decode('utf-8')
-        return json.loads(content)
-    except ClientError as e:
-        return {"error": f"Unable to fetch {key} from S3: {str(e)}"}
-    except json.JSONDecodeError:
-        return {"error": f"Error decoding JSON in {key}"}
-
-# Helper function to create JSON responses
-def create_json_response(body, status_code=200):
-    return Response(
-        body=body,
-        status_code=status_code,
-        headers={'Content-Type': 'application/json'}
-    )
-
-categories = fetch_json_from_s3('api/treasury_yield_all.json')
-print (categories)
+# Check if the request was successful
+if response.status_code == 200:
+    print('Subscription successful:', response.json())
+else:
+    print('Failed to subscribe:', response.status_code, response.text)
